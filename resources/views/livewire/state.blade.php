2bf9ca125e250d2e552e0915d47d5b5a0ce72707
Ahmed-Amer-3ra2y <medo36642@gmail.com>
2024-03-01 19:36:08 +0200

    Done ... I need Id From login and link table Scores with Users

--- resources/views/livewire/state.blade.php
+++ resources/views/livewire/state.blade.php
@@ -5,51 +5,72 @@
         <div class="state_text">
             <p>Sunday</p>
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Sunday Percentage --}}
+            <div class="avarage" style="width: 100%"></div>
+        </div>
         <div class="percentage">00%</div>
     </div>
     <div class="Monday">
         <div class="state_text">
             Monday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Monday Percentage --}}
+            <div class="avarage" style="width: 90%"></div>
+        </div>
         <div class="percentage">00%</div>
     </div>
     <div class="Tuesday">
         <div class="state_text">
             Tuesday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Tuesday Percentage --}}
+            <div class="avarage" style="width: 70%"></div>
+        </div>
         <div class="percentage">00%</div>
     </div>
     <div class="Wednesday">
         <div class="state_text">
             Wednesday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Wednesday Percentage --}}
+            <div class="avarage" style="width: 60%"></div>
+        </div>
         <div class="percentage">00%</div>
     </div>
     <div class="Thursday">
         <div class="state_text">
             Thursday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Thursday Percentage --}}
+            <div class="avarage" style="width: 10%"></div>
+        </div>
         <div class="percentage">00%</div>
-
+        
     </div>
     <div class="Friday">
         <div class="state_text">
             Friday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Friday Percentage --}}
+            <div class="avarage" style="width: 16%"></div>
+        </div>
         <div class="percentage">00%</div>
-
+        
     </div>
     <div class="Saturday">
         <div class="state_text">
             Saturday
         </div>
-        <div class="state_color"></div>
+        <div class="state_color">
+            {{-- Saturday Percentage --}}
+            <div class="avarage" style="width: 40%"></div>
+        </div>
         <div class="percentage">00%</div>
 
     </div>
